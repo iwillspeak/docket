d4a49e6a91b88fbdf56cc5a54286c3ed8d39a08a
Will Speak <lithiumflame@gmail.com>
2017-11-25 09:37:35 +0000

    Add Slugify Function
    This takes an input (title or filename) and convets it into something
    that can be used in a URI.

--- src/util.rs
+++ src/util.rs
@@ -6,8 +6,7 @@ use std::path::Path;
 pub fn read_file_to_string(path: &Path) -> String {
     let mut contents = String::new();
     if let Ok(mut file) = File::open(path) {
-        while let Ok(read) = file.read_to_string(&mut contents)
-        {
+        while let Ok(read) = file.read_to_string(&mut contents) {
             trace!("read {} from {:?}", read, path);
             if read == 0 {
                 break;
@@ -15,4 +14,76 @@ pub fn read_file_to_string(path: &Path) -> String {
         }
     }
     contents
+}
+
+/// Convert a string of arbitrary charactes to a form suitable for use
+/// as an HTML identifier or file name.
+pub fn slugify(input: &str) -> String {
+
+    let to_map = match input.find('-') {
+        Some(offset) => {
+            let (prefix, suffix) = input.split_at(offset + 1);
+            if prefix[..offset].chars().all(|c| c.is_numeric()) {
+                suffix
+            } else {
+                input
+            }
+        }
+        None => input,
+    };
+
+    to_map
+        .chars()
+        .map(|c| match c {
+            c if c.is_whitespace() => '-',
+            '?' | '&' | '=' | '\\' | '/' | '"' => '-',
+            _ => c,
+        })
+        .collect()
+}
+
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn slugify_simple_strings() {
+        assert_eq!("a", slugify("a"));
+        assert_eq!("hello", slugify("hello"));
+        assert_eq!("1000", slugify("1000"));
+    }
+
+    #[test]
+    fn slugify_replaces_space_characters() {
+        assert_eq!("hello-world", slugify("hello world"));
+        assert_eq!("foo-bar-baz", slugify("foo\tbar baz"));
+    }
+
+    #[test]
+    fn slufigy_replaces_url_punctuation() {
+        assert_eq!("foo-bar", slugify("foo&bar"));
+        assert_eq!("foo-bar", slugify("foo\"bar"));
+        assert_eq!("foo-bar", slugify("foo?bar"));
+        assert_eq!("foo-bar", slugify("foo=bar"));
+    }
+
+    #[test]
+    fn slugify_replaces_path_components() {
+        assert_eq!("yop-yop-yop", slugify("yop/yop\\yop"));
+    }
+
+    #[test]
+    fn slugify_allows_unicode() {
+        assert_eq!("€$§±*", slugify("€$§±*"));
+        assert_eq!("ü¬é∆∂", slugify("ü¬é∆∂"));
+    }
+
+    #[test]
+    fn slufigy_removes_leading_numbers() {
+        assert_eq!("hello", slugify("01-hello"));
+        assert_eq!("01a-foo", slugify("01a-foo"));
+        assert_eq!("01a-foo", slugify("01a-foo"));
+    }
 }
