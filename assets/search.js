f562725dc26d818ee8d947764e55932750b0f312
Will Speak <lithiumflame@gmail.com>
2021-06-13 18:51:02 +0100

    Address Code Review Comments
    Thanks James!

--- assets/search.js
+++ assets/search.js
@@ -1,84 +1,89 @@
-// Javascript Progressive Search for Docket
-(function (window, document) {
-    fetch("./search_index.json")
-        .then(response => response.json())
-        .then(searchIndex => {
+// # Javascript Progressive Search for Docket
+//
+// This file loads a search index that's pre-baked by `docket` at documentation
+// compile time. If the search index loads we inject the search box into the
+// page and enable the search.
+fetch("./search_index.json")
+    .then(response => response.json())
+    .then(searchIndex => {
 
-            let searchForm = document.querySelector("#docket-search");
-            if (searchForm == null) {
+        const searchForm = document.querySelector("#docket-search");
+        if (searchForm == null) {
+            return;
+        }
+
+        
+        searchForm.innerHTML = '<h2>Search</h2><form target="none"><input id="query" autofocus="" placeholder="enter your search" type="search"><button action="submit">Search</button></form><div id="docket-search-results"></div>';
+        const searchBox = searchForm.querySelector('#query');
+        const searchResults = searchForm.querySelector('#docket-search-results');
+
+        let searchEntryForResult = function (result) {
+            return `<li><a class="search-result" href="${result.slug}/" >${result.title}</a></li>`;
+        }
+
+        let displayResults = function (results) {
+            if (results.length == 0) {
+                searchResults.innerHTML = "<h3>No results</h3>";
+            } else {
+                searchResults.innerHTML =
+                    `<h3>${results.length} results</h3>
+                    <ul class="search-results">
+                        ${results.map(searchEntryForResult).join('')}
+                    </ul>`;
+            }
+        }
+
+        let doSearch = function (query) {
+
+            // If the search is empty clean up.
+            if (query.trim().length == 0) {
+                searchResults.innerHTML = "";
                 return;
             }
 
-            searchForm.innerHTML = '<h2>Search</h2><form target="none"><input id="query" autofocus="" placeholder="enter your search" type="search"><button action="submit">Search</button></form><div id="docket-search-results"></div>';
-            searchBox = searchForm.querySelector('#query');
-            searchResults = searchForm.querySelector('#docket-search-results');
+            let terms = query.split(/[^\w]/)
+                .map(term => term.trim().toLowerCase())
+                .filter(term => term.length > 0);
+            let found = []
 
-            let searchEntryForResult = function (result) {
-                return `<li><a class="search-result" href="${result.slug}/" >${result.title}</a></li>`;
-            }
-
-            let displayResults = function (results) {
-                if (results.length == 0) {
-                    searchResults.innerHTML = "<h3>No results</h3>";
-                } else {
-                    searchResults.innerHTML =
-                        `<h3>${results.length} results</h3><ul class="search-results">`
-                        + results.map(searchEntryForResult).join('') + '</ul>';
-                }
-            }
-
-            let doSearch = function (query) {
-
-                // If the search is empty clean up.
-                if (query.trim().length == 0) {
-                    searchResults.innerHTML = "";
-                    return;
-                }
-
-                let terms = query.split(/[^\w]/)
-                    .map(term => term.trim().toLowerCase())
-                    .filter(term => term.length > 0);
-                let found = []
-
-                searchIndex.forEach(page => {
-                    let score = 0;
-                    terms.forEach(term => {
-                        let termScore = page.terms[term];
-                        if (termScore !== undefined) {
-                            score += termScore;
-                        }
-                    });
-                    if (score > 0) {
-                        found.push({
-                            score: score,
-                            page: page,
-                        });
+            searchIndex.forEach(page => {
+                let score = 0;
+                terms.forEach(term => {
+                    let termScore = page.terms[term];
+                    if (termScore !== undefined) {
+                        score += termScore;
                     }
                 });
-
-                // Order them by the score.
-                found.sort((a, b) => a.score < b.score);
-
-                displayResults(found.map(f => f.page));
-            }
-
-            let timer = null;
-            searchBox.addEventListener('keyup', function (event) {
-                if (timer == null) {
-                    timer = setTimeout(function () {
-                        timer = null;
-                        doSearch(searchBox.value);
-                    }, 500);
+                if (score > 0) {
+                    found.push({
+                        score: score,
+                        page: page,
+                    });
                 }
             });
 
-            searchForm.addEventListener('submit', function (event) {
-                event.preventDefault();
-                if (timer !== null) {
-                    clearTimeout(timer);
+            // Order them by the score, descending.
+            found.sort((a, b) => b.score - a.score);
+
+            displayResults(found.map(f => f.page));
+        }
+
+        let timer = null;
+        searchBox.addEventListener('keyup', function (event) {
+            if (timer == null) {
+                timer = setTimeout(function () {
                     timer = null;
-                }
-                doSearch(searchBox.value);
-            });
+                    doSearch(searchBox.value);
+                }, 500);
+            }
         });
-})(window, document)
+
+        searchForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            if (timer !== null) {
+                clearTimeout(timer);
+                timer = null;
+            }
+            doSearch(searchBox.value);
+        });
+    });
