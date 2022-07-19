97accc75b9f649936ec0b6359747f10c1c00ac33
Will Speak <lithiumflame@gmail.com>
2022-07-19 08:29:06 +0100

    Revive Watch Support
    Brings back the support for watching and rebuilding directories.

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,30 @@ mod render;
 mod search;
 mod utils;
 
-use std::{error::Error, path::PathBuf, process::exit};
+use std::{
+    error::Error,
+    fs,
+    path::{Path, PathBuf},
+};
 
 use docket::Docket;
 use error::Result;
-use log::{info, warn};
+use log::{error, info, warn};
+
+use crate::error::ResultExt;
 
 #[cfg(all(feature = "syntect-hl", legacy))]
 #[macro_use]
 extern crate lazy_static;
 
-/// Run a single pass of documentation generation
-fn run(source: PathBuf, target: PathBuf) -> Result<()> {
-    info!("Rendering documenation from {:?} => {:?}", &source, &target);
-    Docket::open(source)?.render(target)?;
-    Ok(())
+/// On Error Behaviour
+///
+/// Chooses what should happen if an error happens when running the build.
+#[derive(PartialEq, Copy, Clone)]
+#[cfg_attr(not(feature = "watch"), allow(dead_code))]
+enum OnError {
+    Ignore,
+    Exit,
 }
 
 fn main() {
@@ -37,13 +46,97 @@ fn main() {
     let source = utils::path_or_default(args.flag_source, ".");
     let target = utils::path_or_default(args.flag_target, "build/");
 
-    if let Err(err) = run(source, target) {
+    handle_err(
+        if args.flag_watch {
+            watch_and_build(&target, &source)
+        } else {
+            build(&source, &target)
+        },
+        OnError::Exit,
+    )
+}
+
+/// Watch and Rebuild
+///
+/// This opens a file watcher listening for changes in the source directory.
+/// When a file is changed we re-build the documentaiton tree.
+fn watch_and_build(target: &PathBuf, source: &PathBuf) -> Result<()> {
+    #[cfg(feature = "watch")]
+    {
+        use notify::{watcher, RecursiveMode, Watcher};
+        use std::sync::mpsc::channel;
+        use std::time::Duration;
+
+        // Create the target directory first, to ensure we can unwatch it if
+        // needed.
+        //
+        // TODO: properly handle the error here.
+        fs::create_dir_all(target).annotate_err("Error creating target directory")?;
+
+        let (tx, rx) = channel();
+
+        let mut watcher =
+            watcher(tx, Duration::from_secs(2)).annotate_err("could not create file watcher")?;
+
+        watcher
+            .watch(source, RecursiveMode::Recursive)
+            .annotate_err("Error watching source directory")?;
+        // Ignore the erorr here. It most likely means the source and target dir
+        // don't overlap.
+        let _ = watcher.unwatch(target);
+
+        handle_err(build(source, target), OnError::Ignore);
+        println!("Build complete. Watching for changes.");
+        loop {
+            match rx.recv() {
+                Ok(s) => match s {
+                    // Ignore these. They're not debounced.
+                    notify::DebouncedEvent::NoticeWrite(_)
+                    | notify::DebouncedEvent::NoticeRemove(_) => (),
+                    // Write something out if we notice erorrs.
+                    notify::DebouncedEvent::Error(e, path) => {
+                        warn!("Watcher error at path {path:?}: {e}")
+                    }
+                    // Anything else means a rebuild
+                    _ => {
+                        println!("Rebuilding...");
+                        handle_err(build(source, target), OnError::Ignore);
+                        println!("Rebuild complete. Watching for changes.");
+                    }
+                },
+                // println!("Rebuilding... {s:?}")
+                Err(e) => eprintln!("Watcher error: {}", e),
+            }
+        }
+    }
+    #[cfg(not(feature = "watch"))]
+    {
+        eprintln!("Watch not supported. Performing on off build.");
+        build(source, target)
+    }
+}
+
+/// Display the contents of the error, if any, and maybe exit
+fn handle_err(err: Result<()>, on_err: OnError) {
+    if let Err(err) = err {
         eprintln!("docket: error: {}", err);
         if let Some(source) = err.source() {
             warn!("Error caused by {}", source);
         }
-        exit(-1);
+        if on_err == OnError::Exit {
+            std::process::exit(-1);
+        }
     }
+}
+
+/// Run a single pass of documentation generation
+///
+/// This does the main job of rendering the documentaiton. Seprated into a
+/// different function so we can use the `?` operator, and repeatedly call if
+/// we're watching files and re-rendering on change.
+fn build(source: &Path, target: &Path) -> Result<()> {
+    info!("Rendering documenation from {:?} => {:?}", &source, &target);
+    Docket::open(source)?.render(target)
 }
 
 /// Initialise logging
